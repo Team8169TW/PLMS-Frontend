cb2e058b378e9d0fe2c755dfbac9bdc8e0cc9b62
Sz-Wei Chen <swchen1217@gmail.com>
2022-01-06 21:01:32 +0800

    202201062101

--- src/components/PartsCard.jsx
+++ src/components/PartsCard.jsx
@@ -8,40 +8,44 @@ export default function PartsCard(props) {
     <Card>
       <Card.Body className="p-3">
         <Card.Title>零件資訊</Card.Title>
-        <Table striped hover size="sm" borderless className="mb-0">
-          <tbody>
-            <tr>
-              <th style={{ whiteSpace: "nowrap" }}>序號</th>
-              <td>P.{partsInfo.id}</td>
-              <th style={{ whiteSpace: "nowrap" }}>類別</th>
-              <td>{partsInfo.category}</td>
-            </tr>
-            <tr>
-              <th>名稱</th>
-              <td>{partsInfo.common_name}</td>
-              <th>規格</th>
-              <td>{partsInfo.spec}</td>
-            </tr>
-            <tr>
-              <th>廠商</th>
-              <td>{partsInfo.supplier_name}</td>
-              <th>料號</th>
-              <td>{partsInfo.product_code}</td>
-            </tr>
-            <tr>
-              <th>料名</th>
-              <td colSpan="3">{partsInfo.product_name}</td>
-            </tr>
-            <tr>
-              <th>數量</th>
-              <td>
-                {partsInfo.quantity} {partsInfo.unit}
-              </td>
-              <th>備註</th>
-              <td>{partsInfo.note}</td>
-            </tr>
-          </tbody>
-        </Table>
+        {partsInfo.id === 0 ? (
+          <p className="m-0 p-2 padding">等待輸入...</p>
+        ) : (
+          <Table striped hover size="sm" borderless className="mb-0">
+            <tbody>
+              <tr>
+                <th style={{ whiteSpace: "nowrap" }}>序號</th>
+                <td>{partsInfo.id !== 0 && `P.${partsInfo.id}`}</td>
+                <th style={{ whiteSpace: "nowrap" }}>類別</th>
+                <td>{partsInfo.category}</td>
+              </tr>
+              <tr>
+                <th>名稱</th>
+                <td>{partsInfo.common_name}</td>
+                <th>規格</th>
+                <td>{partsInfo.spec}</td>
+              </tr>
+              <tr>
+                <th>廠商</th>
+                <td>{partsInfo.supplier_name}</td>
+                <th>料號</th>
+                <td>{partsInfo.product_code}</td>
+              </tr>
+              <tr>
+                <th>料名</th>
+                <td colSpan="3">{partsInfo.product_name}</td>
+              </tr>
+              <tr>
+                <th>數量</th>
+                <td>
+                  {partsInfo.quantity} {partsInfo.unit}
+                </td>
+                <th>備註</th>
+                <td>{partsInfo.note}</td>
+              </tr>
+            </tbody>
+          </Table>
+        )}
       </Card.Body>
     </Card>
   );
@@ -50,14 +54,14 @@ export default function PartsCard(props) {
 PartsCard.propTypes = {
   partsInfo: PropTypes.shape({
     id: PropTypes.number.isRequired,
-    category: PropTypes.string.isRequired,
-    common_name: PropTypes.string.isRequired,
-    spec: PropTypes.string.isRequired,
-    quantity: PropTypes.number.isRequired,
-    unit: PropTypes.string.isRequired,
-    supplier_name: PropTypes.string.isRequired,
-    product_name: PropTypes.string.isRequired,
-    product_code: PropTypes.string.isRequired,
-    note: PropTypes.string.isRequired,
+    category: PropTypes.string,
+    common_name: PropTypes.string,
+    spec: PropTypes.string,
+    quantity: PropTypes.number,
+    unit: PropTypes.string,
+    supplier_name: PropTypes.string,
+    product_name: PropTypes.string,
+    product_code: PropTypes.string,
+    note: PropTypes.string,
   }).isRequired,
 };
--- src/components/StoreCard.jsx
+++ src/components/StoreCard.jsx
@@ -8,35 +8,39 @@ export default function StoreCard(props) {
     <Card>
       <Card.Body className="p-3">
         <Card.Title>倉儲資訊</Card.Title>
-        <Table striped hover size="sm" borderless className="mb-0">
-          <tbody>
-            <tr>
-              <th style={{ whiteSpace: "nowrap" }}>編號</th>
-              <td>
-                S.{storeInfo.area_code}-{storeInfo.grid_number}-
-                {storeInfo.box_number}
-              </td>
-              <th style={{ whiteSpace: "nowrap" }}>櫃號</th>
-              <td>
-                {storeInfo.area_code} ({storeInfo.area_name})
-              </td>
-            </tr>
-            <tr>
-              <th>層號</th>
-              <td>
-                {storeInfo.grid_number === 0
-                  ? "－未指定－"
-                  : `第 ${storeInfo.grid_number} 層 (${storeInfo.grid_name})`}
-              </td>
-              <th>格號</th>
-              <td>
-                {storeInfo.box_number === 0
-                  ? "－未指定－"
-                  : `第 ${storeInfo.box_number} 格`}
-              </td>
-            </tr>
-          </tbody>
-        </Table>
+        {storeInfo.area_code === "NULL" ? (
+          <p className="m-0 p-2 padding">等待輸入...</p>
+        ) : (
+          <Table striped hover size="sm" borderless className="mb-0">
+            <tbody>
+              <tr>
+                <th style={{ whiteSpace: "nowrap" }}>編號</th>
+                <td>
+                  S.{storeInfo.area_code}-{storeInfo.grid_number}-
+                  {storeInfo.box_number}
+                </td>
+                <th style={{ whiteSpace: "nowrap" }}>櫃號</th>
+                <td>
+                  {storeInfo.area_code} ({storeInfo.area_name})
+                </td>
+              </tr>
+              <tr>
+                <th>層號</th>
+                <td>
+                  {storeInfo.grid_number === 0
+                    ? "－未指定－"
+                    : `第 ${storeInfo.grid_number} 層 (${storeInfo.grid_name})`}
+                </td>
+                <th>格號</th>
+                <td>
+                  {storeInfo.box_number === 0
+                    ? "－未指定－"
+                    : `第 ${storeInfo.box_number} 格`}
+                </td>
+              </tr>
+            </tbody>
+          </Table>
+        )}
       </Card.Body>
     </Card>
   );
@@ -45,9 +49,9 @@ export default function StoreCard(props) {
 StoreCard.propTypes = {
   storeInfo: PropTypes.shape({
     area_code: PropTypes.string.isRequired,
-    area_name: PropTypes.string.isRequired,
-    grid_number: PropTypes.number.isRequired,
-    grid_name: PropTypes.string.isRequired,
-    box_number: PropTypes.number.isRequired,
+    area_name: PropTypes.string,
+    grid_number: PropTypes.number,
+    grid_name: PropTypes.string,
+    box_number: PropTypes.number,
   }).isRequired,
 };
